32bd3aa9e29ece0ec12dac5e18269e7059aa073d
YuraDnistriansky <yura.dniester@gmail.com>
2025-12-23 23:16:35 +0100

    connect relay

--- src/initPins.cpp
+++ src/initPins.cpp
@@ -1,6 +1,10 @@
+#include <Arduino.h>
+
 #include "../include/initPins.hpp"
+
 #include "../include/config.hpp"
 
 void initPins() {
-    
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH);
 }
